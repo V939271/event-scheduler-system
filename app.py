21c8791c4ee8fccf9ffdb4db88a8f6a2c94378c1
venkat <venkat.tanneru92@gmail.com>
2025-06-29 13:58:03 +0530

    Update: Started reminder thread in app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, request, jsonify
 from utils import load_events, save_events, find_event_by_id
 import uuid
 from datetime import datetime
+from reminder import check_reminders  
+import threading
 
 app = Flask(__name__)
 events = load_events()
 
-@app.route("/events", methods=["POST"])
+@app.route("/events", methods=["POST"]) # signature is /events { }
 def create_event():
-    data = request.get_json()
+    data = request.get_json() # here request is read json body in postman
     event = {
         "id": str(uuid.uuid4()),
         "title": data["title"],
@@ -17,17 +19,17 @@ def create_event():
         "end_time": data["end_time"]
     }
     events.append(event)
-    save_events(events)
-    return jsonify({"message": "Event created", "event": event}), 201
+    save_events(events) # here we all call the save_events funtion and pass the events data and saved into file
+    return jsonify({"message": "Event created", "event": event}), 201  # convert the dict data into json format
 
 @app.route("/events", methods=["GET"])
 def list_events():
-    sorted_events = sorted(events, key=lambda x: x["start_time"])
+    sorted_events = sorted(events, key=lambda x: x["start_time"]) # to Sort the data we used lambda fun
     return jsonify(sorted_events)
 
 @app.route("/events/<event_id>", methods=["PUT"])
-def update_event(event_id):
-    event = find_event_by_id(events, event_id)
+def update_event(event_id): # to
+    event = find_event_by_id(events, event_id) # this event is getting form api <event_id>
     if not event:
         return jsonify({"error": "Event not found"}), 404
     data = request.get_json()
@@ -37,10 +39,13 @@ def update_event(event_id):
 
 @app.route("/events/<event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    global events
-    events = [e for e in events if e["id"] != event_id]
+    #global events
+    events = [e for e in events if e["id"] != event_id] 
     save_events(events)
     return jsonify({"message": "Event deleted"}), 200
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": 
+    app.run(debug=True) # To run app 
+
+reminder_thread = threading.Thread(target=check_reminders, daemon=True)
+reminder_thread.start()
